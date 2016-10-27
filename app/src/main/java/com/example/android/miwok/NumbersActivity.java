4a983d85d66d0e06eb2505ea333a56921d13b75f
real73 <real1107055@gmail.com>
2016-10-27 18:22:40 +0600

    Lesson 2 - Dynamically row adding using for loop

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,6 +13,38 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
-        //setTitle("Numbers");
+
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        // Find the root view so we can add child views to it
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.activity_numbers);
+
+        // Create a variable to keep track of the current index position
+
+
+        // Keep looping until we've reached the end of the list (which means keep looping
+        // as long as the current index position is less than the length of the list)
+        for (int count = 0;count<words.size();count++){
+
+            // Create a new TextView
+            TextView tvNumberText = new TextView(this);
+
+            // Set the text to be word at the current index
+            tvNumberText.setText(words.get(count));
+
+            // Add this TextView as another child to the root view of this layout
+            rootView.addView(tvNumberText);
+            
+        }
     }
 }
