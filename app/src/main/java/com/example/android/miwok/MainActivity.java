68c50541aa01361c1a2ad4c4016f690d636b431e
real73 <real1107055@gmail.com>
2016-10-27 14:45:22 +0600

    Using OnclikListener And label at Manifest

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,10 +15,16 @@
  */
 package com.example.android.miwok;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Intent intent;
+    TextView tvNumbers,tvFamily,tvColors,tvPhrases;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +32,51 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+        componentInit();
+        addListener();
     }
+
+    private void addListener() {
+        tvNumbers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(MainActivity.this, NumbersActivity.class);
+                startActivity(intent);
+                Toast.makeText(MainActivity.this, "Numbers Activity ", Toast.LENGTH_SHORT).show();
+            }
+        });
+        tvFamily.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(MainActivity.this, FamilyActivity.class);
+                startActivity(intent);
+                Toast.makeText(MainActivity.this, "Family Activity", Toast.LENGTH_SHORT).show();
+            }
+        });
+        tvColors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(MainActivity.this, ColorsActivity.class);
+                startActivity(intent);
+                Toast.makeText(MainActivity.this, "Colors Activity ", Toast.LENGTH_SHORT).show();
+            }
+        });
+        tvPhrases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(MainActivity.this, PhrasesActivity.class);
+                startActivity(intent);
+                Toast.makeText(MainActivity.this, "Phrases Activity", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void componentInit() {
+        tvNumbers=(TextView) findViewById(R.id.numbers);
+        tvFamily=(TextView) findViewById(R.id.family);
+        tvColors=(TextView) findViewById(R.id.colors);
+        tvPhrases=(TextView) findViewById(R.id.phrases);
+    }
+
+
 }
